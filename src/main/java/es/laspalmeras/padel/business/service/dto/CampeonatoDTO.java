8588b82c3d19ba82092ebfdc6316c250747618b7
edujg81 <edujg81@gmail.com>
2024-07-01 11:21:39 +0200

    Se modifica el DTO campeonato para hacer atributos obligatorios

--- src/main/java/es/laspalmeras/padel/business/service/dto/CampeonatoDTO.java
+++ src/main/java/es/laspalmeras/padel/business/service/dto/CampeonatoDTO.java
@@ -1,15 +1,28 @@
 package es.laspalmeras.padel.business.service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class CampeonatoDTO {
     private Long id;
+    
+    @NotBlank
     private Integer year;
+    
+    @NotBlank
+    @Pattern(regexp = "Masculino|Femenino|Mixto", message = "Categoría debe ser 'Masculino', 'Femenino' o 'Mixto'")
     private String categoria;
+    
+    @NotBlank
+    @Pattern(regexp = "\\d+", message = "División debe ser un número entero")
     private Integer division;
-    private String estado;
-    private Boolean activo;
-    private Integer puntosPorVictoria;
-    private Integer puntosPorDerrota;
+    
+    @Pattern(regexp = "Sin iniciar|En curso|Finalizado", message = "Estado debe ser 'Sin iniciar', 'En curso' o 'Finalizado'")
+    private String estado = "Sin iniciar";
+    
+    private Boolean activo = true;
+    private Integer puntosPorVictoria = 2;
+    private Integer puntosPorDerrota = 0;
 }
