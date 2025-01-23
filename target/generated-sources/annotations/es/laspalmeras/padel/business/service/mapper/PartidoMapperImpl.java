9e8e8e4a58a9769fc6562a768c34aed1e0486725
edujg81 <edujg81@gmail.com>
2025-01-23 15:48:43 +0100

    backup tras instalación nuevo proyecto

--- target/generated-sources/annotations/es/laspalmeras/padel/business/service/mapper/PartidoMapperImpl.java
+++ target/generated-sources/annotations/es/laspalmeras/padel/business/service/mapper/PartidoMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-20T11:05:21+0100",
+    date = "2025-01-23T15:41:30+0100",
     comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.40.0.v20241112-0530, environment: Java 21.0.5 (Eclipse Adoptium)"
 )
 @Component
