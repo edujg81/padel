ee72b8d0c0826bc1a57ade34e0f6814031cfc3cf
edujg81 <edujg81@gmail.com>
2025-02-05 15:16:33 +0100

    se añade descripción a endpoint de ausencias

--- src/main/java/es/laspalmeras/padel/controller/AusenciaController.java
+++ src/main/java/es/laspalmeras/padel/controller/AusenciaController.java
@@ -2,12 +2,14 @@ package es.laspalmeras.padel.controller;
 
 import es.laspalmeras.padel.dto.AusenciaDTO;
 import es.laspalmeras.padel.service.AusenciaService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public/ausencias")
+@Tag(name = "Gestión de Ausencia", description = "Operaciones pertenecientes a ausencias en Club de Padel")
 public class AusenciaController {
 
     private final AusenciaService ausenciaService;
